1796c0c200aff0a201195b7d3cfb8e08baf9777d
Richard May <mybrainishuge@users.noreply.github.com>
2016-04-20 14:47:51 -0700

    Complete functional class prompts

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    length && length--;
+    return storage[length];
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
