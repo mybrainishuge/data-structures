674b938c5710940bda6c2c7da589786770e5b21e
mybrainishuge <richardamay@gmail.com>
2016-04-21 14:26:26 -0700

    Pass five tests on tree prompt

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = {};
 
   return newTree;
 };
@@ -11,9 +10,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var keys = Object.keys(this.children).length;
+  this.children[keys] = Object.create(treeMethods);
+  this.children[keys].value = value;
+  this.children[keys].children = {};
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  for (var child in this.children) {
+    if (child.value === target) {
+      return true;
+    }
+    if (!Object.keys(child.children).length) {
+      return child.contains(target);
+    }
+  }
+  return false;
 };
 
 
@@ -21,3 +37,32 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/*
+  var tree = Tree();
+
+  tree = {
+    value: undefined,
+    children: {
+      0: {
+        value: 5,
+        children: {
+          0: {
+            value: 7,
+            children: {}
+          }
+        }
+      },
+
+      1: {
+        value: 6,
+        children: {
+          0: {
+            value: 8,
+            children: {}
+          }
+        }
+      }
+    },
+  }
+*/
