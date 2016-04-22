bc19dfb62e3a3f425e174001b941c1b2a5dbb62f
mybrainishuge <richardamay@gmail.com>
2016-04-22 12:10:52 -0700

    Pass tests for hashTable prompt

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,43 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (Array.isArray(bucket)) {
+    bucket.push([k, v]);
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  return bucket.reduce(function(result, tuple) {
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+    return result;
+  }, undefined);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  bucket.forEach(function(tuple, idx) {
+    if (tuple[0] === k) {
+      bucket.splice(idx, 1);
+    }
+  });
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ The overall HashTable is constant time, but contains certain elements that are linear in nature.
+ The `insert` method is constant time.
+ The `retrieve` method is linear.
+ The `remove` method is linear.
  */
 
 
